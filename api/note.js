1792ccc6bcb9118dc6924937939bf0d5d5dd7e33
asger991 <sirjazzper@gmail.com>
2023-02-01 14:44:49 +0100

    -1

--- api/note.js
+++ api/note.js
@@ -1,9 +1,9 @@
-import { tokenizeString } from "./_utils";
-import { setKey } from "./slash_handlers/_set_key";
-import { addToList } from "./slash_handlers/_add_to_list";
-import { listAll } from "./slash_handlers/_list_all";
-import { getKey } from "./slash_handlers/_get_key";
-import { removeFromList } from "./slash_handlers/_remove_from_list";
+import { tokenizeString } from './_utils';
+import { setKey } from './slash_handlers/_set_key';
+import { addToList } from './slash_handlers/_add_to_list';
+import { listAll } from './slash_handlers/_list_all';
+import { getKey } from './slash_handlers/_get_key';
+import { removeFromList } from './slash_handlers/_remove_from_list';
 
 module.exports = async (req, res) => {
   // console.log("req::::", req);
@@ -13,29 +13,37 @@ module.exports = async (req, res) => {
   const action = commandArray[0];
 
   switch (action) {
-    case "set":
+    case 'set':
       setKey(res, commandArray);
       break;
-    case "get":
+    case 'get':
       getKey(res, commandArray);
       break;
-    case "+1":
+    case '+1':
       //get the key
       let value = await getKey(res, commandArray, true);
       //key plus 1
-      const NewCommandArray = ["set", commandArray[1], parseInt(value) + 1];
+      const NewCommandArray = ['set', commandArray[1], parseInt(value) + 1];
       //save key
       setKey(res, NewCommandArray);
       break;
+    case '-1':
+      //get the key
+      let value1 = await getKey(res, commandArray, true);
+      //key plus 1
+      const NewCommandArray1 = ['set', commandArray[1], parseInt(value1) - 1];
+      //save key
+      setKey(res, NewCommandArray1);
+      break;
 
-    case "list-all":
+    case 'list-all':
       listAll(res, commandArray);
       break;
 
     default:
       res.send({
-        response_type: "ephemeral",
-        text: "Wrong usage of the command!",
+        response_type: 'ephemeral',
+        text: 'Wrong usage of the command!',
       });
   }
 };
