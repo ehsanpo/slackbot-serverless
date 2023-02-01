0b2ed378b779637dfdad5956e2c03e07996af413
Ehsan Pourhadi <ehsan.po@gmail.com>
2023-02-01 11:08:05 +0100

    workflow test

--- api/slash_handlers/_set_key.js
+++ api/slash_handlers/_set_key.js
@@ -1,31 +1,30 @@
-const axios = require('axios')
-import { redisURL, redisToken } from '../_constants'
+const axios = require("axios");
+import { redisURL, redisToken } from "../_constants";
 
-export async function setKey(res, commandArray) {
+export async function setKey(res, req, commandArray) {
+  let key = commandArray[1];
+  let value = commandArray[2];
+  let event = req.body.event;
 
-    let key = commandArray[1]
-    let value = commandArray[2]
-
-    await axios({
-        url: `${redisURL}/set/${key}/${value}`,
-        headers: {
-            "Authorization": `Bearer ${redisToken}`
-        }
+  console.log(req);
+  await axios({
+    url: `${redisURL}/set/${key}/${value}`,
+    headers: {
+      Authorization: `Bearer ${redisToken}`,
+    },
+  })
+    .then((response) => {
+      console.log("data from axios:", response.data);
+      res.send({
+        response_type: "in_channel",
+        text: `Successfully set ${key}=${value}`,
+      });
     })
-        .then(response => {
-            console.log("data from axios:", response.data)
-            res.send({
-                "response_type": "in_channel",
-                "text": `Successfully set ${key}=${value}`
-            })
-        })
-        .catch(err => {
-            console.log("axios Error:", err)
-            res.send({
-                "response_type": "ephemeral",
-                "text": `${err.response.data.error}`
-            })
-        })
-
+    .catch((err) => {
+      console.log("axios Error:", err);
+      res.send({
+        response_type: "ephemeral",
+        text: `${err.response.data.error}`,
+      });
+    });
 }
-
